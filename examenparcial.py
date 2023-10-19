2692519f91e3ef9087e9d8c014c79b92a98720b5
EuGarciaa <145038260+EuGarciaa@users.noreply.github.com>
2023-10-19 09:33:46 -0600

    Create examenparcial.py

---
+++ examenparcial.py
@@ -0,0 +1,8 @@
+pregunta = input("¿que hora es?")
+from datetime import datetime
+hora_actual1 = datetime.now().time()
+
+if datetime.strptime:("6:00")
+print("las bebidas se sirven mas rapido")
+else(): datetime.strftime:("12:00")
+print("espera tu turno")
